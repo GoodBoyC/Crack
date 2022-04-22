197affa2254181d24a550d99d9572238ac2f3a3c
Emmanuel Chinda <97448054+GoodBoyC@users.noreply.github.com>
2022-04-22 06:16:57 +0100

    Create run.py

---
+++ run.py
@@ -0,0 +1,15 @@
+import os, sys, platform
+os.system('git pull')
+os.system('termux-setup-storage')
+try:
+
+    import requests
+
+except:
+
+    os.system('pip install requests')
+os.system('xdg-open https://facebook.com/groups/2734614926615884/')
+
+from crack import chinda
+
+qsbuy()
